d540bf3ddfaa26fa84917f9fae125fa46bb6001f
Alex Wang <awang24@students.d125.org>
2023-01-16 09:39:08 -0600

    tokens

--- token/token.go
+++ token/token.go
@@ -1,1 +1,73 @@
 package token
+
+// Declaring TokenType as a type of string
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// Declaring TokenTypes
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + Literals
+	IDENT  = "IDENT"
+	INT    = "INT"
+	STRING = "STRING"
+	FLOAT  = "FLOAT"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	SLASH    = "/"
+	ASTERISK = "*"
+	EXCITE   = "!"
+
+	LT     = "<"
+	GT     = ">"
+	EQUAL  = "=="
+	NEQUAL = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	COLON     = ":"
+
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	// Keywords
+	FUNC   = "FUNC"
+	LET    = "LET"
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	IF     = "IF"
+	ELSE   = "ELSE"
+	RETURN = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNC,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func lookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
